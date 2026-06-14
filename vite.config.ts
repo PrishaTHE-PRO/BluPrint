6376d06064ab8a9a6a07b0a16527a61194f091aa
Aditi Amarnath <aditiamarnath@Aditis-MacBook-Air.local>
2026-06-14 14:07:02 -0500

    still trying

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { "/api": "http://localhost:5000" } },
+  server: { proxy: { "/api": "http://localhost:3001" } },
   build: {
     rollupOptions: {
       input: {
